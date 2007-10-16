eb65caf79e44f96f4cc7d5bc13eba18ce7b00e6f
Takanao.Endoh <Takanao.Endoh@db867174-6e3c-0410-bf4f-d590cd53973e>
2007-10-16 13:03:53 +0000

    Refactored filter.select_anchor

--- trunk/filter/select_anchor.py
+++ trunk/filter/select_anchor.py
@@ -1,6 +1,16 @@
 from StringIO import StringIO
 import habu.log as log
 
+try:
+    from lxml import etree
+    _PARSING_MODULE = "lxml"
+except ImportError:
+    try:
+        from BeautifulSoup import BeautifulSoup
+        _PARSING_MODULE = "BeautifulSoup"
+    except ImportError:
+        raise Warning('ImportError. Required lxml(http://codespeak.net/lxml/) or BeautifulSoup(http://www.crummy.com/software/BeautifulSoup/).')
+
 
 class Main(object):
     def __init__(self, config, environ):
@@ -24,7 +34,10 @@ def create(*argv, **kwargv):
     return Main(*argv, **kwargv)
 
 def get_links(*argv, **kwargv):
-    links = _get_links(*argv, **kwargv)
+    links = dict(
+        lxml = _l_get_links,
+        BeautifulSoup = _b_get_links,
+    ).get(_PARSING_MODULE)(*argv, **kwargv)
     log.info(u"Selected %d anchor(s)" % len(links))
     return links
 
@@ -42,13 +55,3 @@ def _b_get_links(entry):
         links.update({u"%s" % href.get("href"): unicode(href.string)})
         #log.debug(href.attrib.get("href"), href.text.encode("utf-8"))
     return links
-
-try:
-    from lxml import etree
-    _get_links = _l_get_links
-except ImportError:
-    try:
-        from BeautifulSoup import BeautifulSoup
-        _get_links = _b_get_links
-    except ImportError:
-        raise Warning('ImportError. Required lxml(http://codespeak.net/lxml/) or BeautifulSoup(http://www.crummy.com/software/BeautifulSoup/).')
